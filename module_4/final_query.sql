7ff07f2942a6dc8a8ae32bf78a29f06b7a90d738
Alexey Gaskov <agaskov@groupon.com>
2021-09-07 23:20:29 +0100

    Final query

--- module_4/final_query.sql
+++ module_4/final_query.sql
@@ -9,7 +9,17 @@ where departure_airport = 'AAQ'
 
 -- finall query for the resulting dataset
 WITH flight AS (
-    SELECT *
+    SELECT f.flight_id,
+           f.flight_no,
+           f.aircraft_code,
+           f.departure_airport,
+           f.arrival_airport,
+           a.model,
+           f.scheduled_arrival,
+           f.actual_arrival,
+           f.actual_departure,
+           f.scheduled_departure,
+           f.status
     FROM dst_project.flights f
         LEFT JOIN dst_project.aircrafts a ON f.aircraft_code = a.aircraft_code
     ),
@@ -20,11 +30,20 @@ WITH flight AS (
     ),
     ticket_class AS (
         SELECT tf.flight_id,
-            count(CASE WHEN tf.fare_conditions = 'Economy' THEN tf.fare_conditions END) as ticket_economy,
-            count(CASE WHEN tf.fare_conditions = 'Comfort' THEN tf.fare_conditions END) as ticket_comfort,
-            count(CASE WHEN tf.fare_conditions = 'Business' THEN tf.fare_conditions END) as ticket_business
-        FROM dst_project.ticket_flights as tf
+               count(CASE WHEN tf.fare_conditions = 'Economy' THEN tf.fare_conditions END) as ticket_economy,
+               count(CASE WHEN tf.fare_conditions = 'Comfort' THEN tf.fare_conditions END) as ticket_comfort,
+               count(CASE WHEN tf.fare_conditions = 'Business' THEN tf.fare_conditions END) as ticket_business
+        FROM dst_project.ticket_flights AS tf
         GROUP BY 1
+    ),
+    carrier_capacity AS (
+        SELECT count(a.aircraft_code) AS plane_capacity,
+               a.aircraft_code,
+               a.model
+        FROM dst_project.aircrafts a
+        JOIN dst_project.seats s
+        ON a.aircraft_code = s.aircraft_code
+        GROUP BY a.aircraft_code, a.model
     )
     SELECT f.flight_id,
            f.flight_no,
@@ -36,12 +55,31 @@ WITH flight AS (
            tc.ticket_economy ticket_economy,
            tc.ticket_comfort ticket_comfort,
            tc.ticket_business ticket_business,
+           cc.plane_capacity,
            date_part('hour', f.actual_arrival - f.actual_departure) * 60 +
            date_part('minute', f.actual_arrival - f.actual_departure) flight_duration,
            t.total_amount
     FROM flight f
-        LEFT JOIN ticket t on t.flight_id = f.flight_id
-        LEFT JOIN ticket_class tc on tc.flight_id = f.flight_id
-    where departure_airport = 'AAQ'
-      and (date_trunc('month', scheduled_departure) in ('2017-01-01','2017-02-01', '2017-12-01'))
-      and status not in ('Cancelled');
+        JOIN ticket t ON t.flight_id = f.flight_id
+        JOIN ticket_class tc ON tc.flight_id = f.flight_id
+        inner JOIN carrier_capacity cc ON cc.aircraft_code = f.aircraft_code
+    WHERE departure_airport = 'AAQ'
+      AND (date_trunc('month', scheduled_departure) IN ('2017-01-01','2017-02-01', '2017-12-01'))
+      AND status NOT IN ('Cancelled');
+
+
+
+SELECT *
+FROM dst_project.flights f
+LEFT JOIN dst_project.aircrafts a ON f.aircraft_code = a.aircraft_code
+
+
+
+
+SELECT count(a.aircraft_code) as capacity,
+       a.aircraft_code,
+       a.model
+FROM dst_project.aircrafts AS a
+JOIN dst_project.seats AS s
+ON a.aircraft_code = s.aircraft_code
+GROUP BY a.aircraft_code, a.model
