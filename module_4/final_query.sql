77b8b60cbe9406e041d2e6a43c4503f1f8fe7b95
Alexey Gaskov <alexey@Alexeys-MacBook-Pro.local>
2021-09-08 00:15:29 +0100

    formatting

--- module_4/final_query.sql
+++ module_4/final_query.sql
@@ -1,12 +1,3 @@
--- What was given
-select *, (actual_arrival - actual_departure) as duration
-from dst_project.flights
-where departure_airport = 'AAQ'
-  and (date_trunc('month', scheduled_departure) in ('2017-01-01','2017-02-01', '2017-12-01'))
-  and status not in ('Cancelled')
-
-
-
 -- finall query for the resulting dataset
 WITH flight AS (
     SELECT f.flight_id,
@@ -45,24 +36,24 @@ WITH flight AS (
         ON a.aircraft_code = s.aircraft_code
         GROUP BY a.aircraft_code, a.model
     )
-    SELECT f.flight_id,
-           f.flight_no,
-           f.departure_airport,
-           f.arrival_airport,
-           f.model,
-           f.actual_arrival,
-           f.actual_departure,
-           tc.fare_economy fare_economy,
-           tc.fare_comfort fare_comfort,
-           tc.fare_business fare_business,
-           cc.plane_capacity,
-           date_part('hour', f.actual_arrival - f.actual_departure) * 60 +
-           date_part('minute', f.actual_arrival - f.actual_departure) flight_duration,
-           t.total_amount
-    FROM flight f
-        JOIN ticket t ON t.flight_id = f.flight_id
-        JOIN ticket_class tc ON tc.flight_id = f.flight_id
-        inner JOIN carrier_capacity cc ON cc.aircraft_code = f.aircraft_code
-    WHERE departure_airport = 'AAQ'
-      AND (date_trunc('month', scheduled_departure) IN ('2017-01-01','2017-02-01', '2017-12-01'))
-      AND status NOT IN ('Cancelled');
+SELECT f.flight_id,
+       f.flight_no,
+       f.departure_airport,
+       f.arrival_airport,
+       f.model,
+       f.actual_arrival,
+       f.actual_departure,
+       tc.fare_economy fare_economy,
+       tc.fare_comfort fare_comfort,
+       tc.fare_business fare_business,
+       cc.plane_capacity,
+       date_part('hour', f.actual_arrival - f.actual_departure) * 60 +
+       date_part('minute', f.actual_arrival - f.actual_departure) flight_duration,
+       t.total_amount
+FROM flight f
+    JOIN ticket t ON t.flight_id = f.flight_id
+    JOIN ticket_class tc ON tc.flight_id = f.flight_id
+    inner JOIN carrier_capacity cc ON cc.aircraft_code = f.aircraft_code
+WHERE departure_airport = 'AAQ'
+  AND (date_trunc('month', scheduled_departure) IN ('2017-01-01','2017-02-01', '2017-12-01'))
+  AND status NOT IN ('Cancelled');
